f40262a6db403f4e35fde1e12361f70ce020c53f
RegiMaria <regimaria015@gmail.com>
2026-09-09 10:12:40 -0300

    fix: trocamos langchain.text_splitter para langchain-text-splitters

--- src/ingest.py
+++ src/ingest.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 
